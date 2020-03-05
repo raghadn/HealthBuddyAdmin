e9413664c032e081cc04dc881747ce3222c11746
raghadn <alnassar.raghad@gmail.com>
2020-03-05 10:24:03 +0300

    First

--- app/src/main/java/com/example/myhealthbuddyadmin/MainActivity.java
+++ app/src/main/java/com/example/myhealthbuddyadmin/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.myhealthbuddyadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button CreateDoctor,Signoutbtn;
+    private BottomNavigationView bottomnav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomnav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                UserMenuSelector(menuItem);
+                return false;
+            }
+        });
 
         CreateDoctor = findViewById(R.id.CD);
         CreateDoctor.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +59,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()) {
 
+            case R.id.nav_profile:
+                Intent intentprofile = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intentprofile);
+                break;
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/myhealthbuddyadmin/ProfileActivity.java
@@ -0,0 +1,132 @@
+package com.example.myhealthbuddyadmin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomnav;
+    CircleImageView userImage;
+    TextView userName;
+    TextView userHos;
+    private FirebaseUser Fuser;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRef,href;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        userImage=findViewById(R.id.userimage);
+        userHos=findViewById(R.id.Hospital);
+        userName=findViewById(R.id.Name);
+
+        mAuth = FirebaseAuth.getInstance();
+        Fuser =mAuth.getCurrentUser();
+
+        bottomnav=(BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomnav.setSelectedItemId(R.id.nav_profile);
+        bottomnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                UserMenuSelector(menuItem);
+                return false;
+            }
+        });
+
+        mRef = FirebaseDatabase.getInstance().getReference().child("Admins").child(Fuser.getUid());
+
+
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String firstname,lastname,username,hos;
+                if (dataSnapshot.exists()){
+                    firstname = dataSnapshot.child("FirstName").getValue().toString();
+                    lastname=dataSnapshot.child("LastName").getValue().toString();
+                    username=firstname+' '+lastname;
+                    hos=dataSnapshot.child("Hospital").getValue().toString();
+
+                    userName.setText(username);
+
+
+
+
+                    href=FirebaseDatabase.getInstance().getReference().child("Hospitals").child(hos);
+                    href.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String hospital;
+                            hospital=dataSnapshot.child("Name").getValue().toString();
+
+                            userHos.setText(hospital);
+                            String imge=dataSnapshot.child("Image").getValue().toString();
+
+
+                            if(dataSnapshot.hasChild("Image")){
+                                Picasso.get().load(imge).into(userImage);
+                               // Glide.with(getApplicationContext()).load(imge).into(userImage);
+                                 }
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.nav_home:
+                Intent intentprofile = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intentprofile);
+                break;
+
+        }
+
+    }
+}
+
+
