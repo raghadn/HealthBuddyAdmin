190a1f014bc2e492521e3e4bdf61eba65d410f4f
Haifa <haifa.almans@gmail.com>
2020-04-20 01:09:56 +0300

    Missing classes from previous push.

---
+++ app/src/main/java/com/example/myhealthbuddyadmin/XRay.java
@@ -0,0 +1,35 @@
+package com.example.myhealthbuddyadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class XRay extends AppCompatActivity {
+
+    Button writeTest;
+    String PatientKey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xray);
+
+        writeTest=findViewById(R.id.writeTest);
+
+        PatientKey = getIntent().getExtras().get("PatientKey").toString();
+
+        ///////////// ---- redirect to activities ---- /////////////
+        writeTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent redirect = new Intent(XRay.this,WriteXRay.class);
+                redirect.putExtra("PatientKey",PatientKey);
+                startActivity(redirect);
+            }
+        });
+
+    }
+}
