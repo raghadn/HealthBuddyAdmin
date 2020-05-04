77197378394db632060ff5adb424921acff0adf4
Nourabllaa <55226897+Nourabllaa@users.noreply.github.com>
2020-05-04 19:55:24 +0300

    Date + bottom nav+ general modification

--- app/src/main/java/com/example/myhealthbuddyadmin/ViewBloodTest.java
+++ app/src/main/java/com/example/myhealthbuddyadmin/ViewBloodTest.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
